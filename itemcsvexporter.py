f8f075c82f8c5706e6e0958df9fc33820eca963e
acredjb <31613674+acredjb@users.noreply.github.com>
2018-08-23 22:48:57 +0800

    Update itemcsvexporter.py

--- itemcsvexporter.py
+++ itemcsvexporter.py
@@ -1,7 +1,7 @@
 from scrapy.conf import settings
 from scrapy.contrib.exporter import CsvItemExporter
 from scrapy.exporters import CsvItemExporter
-#指定输出到csv文件中字段的顺序，结合setting.py
+
 class itemcsvexporter(CsvItemExporter):
 
     def __init__(self, *args, **kwargs):
